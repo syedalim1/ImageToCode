29cf1083e5dfd3f24bb693288c660519f842e48d
SYED ALI M <143418859+syedalim1@users.noreply.github.com>
2025-04-07 22:38:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["drizzle-orm",
-useFileSystemPublicRoutes: false,
  "@neondatabase/serverless"],
   trailingSlash: true, // Optional: Avoid 404s for subpages
 };
