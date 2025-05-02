4c6fb65cb90a2821d43e457ec6e9b8d13eafdc4f
Your Name <your.email@example.com>
2025-05-02 23:24:47 +0530

    sample

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["drizzle-orm",
- "@neondatabase/serverless"],
+  serverExternalPackages: ["drizzle-orm", "@neondatabase/serverless"],
   trailingSlash: true, // Optional: Avoid 404s for subpages
+};
+module.exports = {
+  amplify: {
+    timeout: 60, // Increases timeout to 60 seconds (max allowed)
+  },
 };
 
 export default nextConfig;
